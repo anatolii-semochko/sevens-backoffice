1176bbdcaf07fc3c54b77593254b118469f46676
anatolii <anatoliy.semochko@gmail.com>
2025-07-05 21:10:46 +0300

    Add help files generation

--- api/src/Service/PagesContent/HelpGenerateService.php
+++ api/src/Service/PagesContent/HelpGenerateService.php
@@ -2,23 +2,50 @@
 
 namespace App\Service\PagesContent;
 
+use App\Repository\Help\HelpContentRepository;
+use App\Repository\Help\HelpRepository;
 use App\Repository\LanguageRepository;
-use App\Service\LanguageService;
-use Doctrine\ORM\EntityManagerInterface;
 
 readonly class HelpGenerateService
 {
     public function __construct(
-        private EntityManagerInterface $em,
-        private LanguageService $languageService,
+        private string $helpTranslationsFolder,
         private LanguageRepository $languageRepository,
+        private HelpRepository $helpRepository,
+        private HelpContentRepository $helpContentRepository,
     ) {}
 
     public function generate(): string
     {
+        $helps = $this->helpRepository->findAll();
         $languages = $this->languageRepository->findAll();
-        $count = count($languages);
 
-        return "Generated $count files";
+        foreach ($languages as $language) {
+            $data = [];
+            $locale = $language->getCode();
+            foreach ($helps as $help) {
+                $helpContent = $this->helpContentRepository->findOneBy([
+                    'language' => $language,
+                    'help' => $help,
+                ]);
+                $data[$help->getName()] = [
+                    'pageUrl' => $help->getPageUrl(),
+                    'title' => $helpContent?->getTitle(),
+                    'shortDescription' => $helpContent?->getShortDescription(),
+                ];
+            }
+            $this->saveFile($locale, $data);
+        }
+
+        return 'Generated ' . count($languages) . ' files';
+    }
+
+    private function saveFile(string $locale, array $data): void
+    {
+        $filePath = $this->helpTranslationsFolder . DIRECTORY_SEPARATOR . "help.{$locale}.json";
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+        file_put_contents($filePath, json_encode($data));
     }
 }
